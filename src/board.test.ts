4840678ec8b96c6405c8134fc711141716ea3b52
Javier González <jgestarriol@gmail.com>
2023-05-14 11:55:31 +0200

    Test randomBoardGenerator function

--- src/board.test.ts
+++ src/board.test.ts
@@ -3,14 +3,28 @@ import { emptyBoardGenerator, randomBoardGenerator } from "./board";
 describe("Given the function emptyBoardGenerator", () => {
   describe("When given the parameters (3, 3)", () => {
     test("Then it should return an array of three arrays, with 3 index each", () => {
+      const columns = 3;
+      const rows = 3;
+
       const expectedResult = [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],
       ];
-      const expectedValue = emptyBoardGenerator(3, 3);
+      const expectedValue = emptyBoardGenerator(columns, rows);
 
       expect(expectedResult).toStrictEqual(expectedValue);
+    });
+  });
+});
+
+describe("Given the function randomBoardGenerator", () => {
+  describe("When given the parameters (3, 3)", () => {
+    test("Then it should return an array of three arrays, with 3 index each", () => {
+      const columns = 3;
+      const rows = 3;
+
+      expect(randomBoardGenerator(columns, rows)).toHaveLength(3);
     });
   });
 });
